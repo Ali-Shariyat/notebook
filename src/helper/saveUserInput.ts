4ec756a999d7dbadfb6aee094807316952d17b00
Shariyat-uu <a.shariyat.78.99.20@gmail.com>
2025-01-04 20:54:45 +0330

    feat: check for file existence

--- src/helper/saveUserInput.ts
+++ src/helper/saveUserInput.ts
@@ -1,6 +1,7 @@
 import * as fs from "fs";
 import promptForAdditionalUser from "./promptForAdditionalUser";
 import readUserDataFromFile from "./readUserDataFromFile";
+import getUserInput from "./getUserInput";
 
 interface SaveUserInputRaw {
   name: string;
@@ -9,12 +10,33 @@ interface SaveUserInputRaw {
 
 const users = readUserDataFromFile<SaveUserInputRaw>();
 
-export default async function saveUserInput({ name, phone }: SaveUserInputRaw) {
-  users.push({ name, phone });
+const checkPhoneExist = (phone: string): boolean =>
+  users.some((user) => user.phone === phone);
+
+const writeUserDataToFile = (user: SaveUserInputRaw): void => {
+  users.push(user);
   try {
     fs.writeFileSync("user.json", JSON.stringify(users, null, 2));
     promptForAdditionalUser();
-  } catch (writeErr) {
-    console.error("Error writing to file", writeErr);
+  } catch (error) {
+    console.error("Error writing to file", error);
   }
+};
+
+const handlePhoneExistence = async (user: SaveUserInputRaw): Promise<void> => {
+  if (checkPhoneExist(user.phone)) {
+    console.log("Phone number already exists");
+    const newPhone = await getUserInput({
+      label: "Choose another phone number:",
+    });
+    await handlePhoneExistence({ ...user, phone: newPhone });
+  } else {
+    writeUserDataToFile(user);
+  }
+};
+
+export default async function saveUserInput(
+  user: SaveUserInputRaw
+): Promise<void> {
+  await handlePhoneExistence(user);
 }
