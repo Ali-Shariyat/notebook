702cc99d89a28be066be9a2a7c766fd3a9409ed0
Shariyat-uu <a.shariyat.78.99.20@gmail.com>
2025-01-06 22:19:11 +0330

    improve: modify return value

--- src/helper/checkPhoneExistence.ts
+++ src/helper/checkPhoneExistence.ts
@@ -2,9 +2,7 @@ import messages from "../data/messages";
 import { isPhoneExist } from "./processFile";
 
 export function checkPhoneExistence(phone: number): boolean {
-  if (isPhoneExist(phone)) {
-    console.log(messages.phoneExists);
-    return false;
-  }
-  return true;
+  const phoneExist = isPhoneExist(phone);
+  if (phoneExist) console.log(messages.phoneExists);
+  return !phoneExist;
 }
