3fba4114a9b54b108dfefc5b428a370f469ad24e
Shariyat-uu <a.shariyat.78.99.20@gmail.com>
2025-01-06 22:22:22 +0330

    fix: remove useless collectUserData file

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,19 @@
+import messages from "./data/messages";
 import askForAnotherData from "./helper/askForAnotherData";
 import { checkPhoneExistence } from "./helper/checkPhoneExistence";
-import { collectUserName, collectUserPhone } from "./helper/collectUserData";
 import { FILE_NAME, writeFile } from "./helper/processFile";
+import { prompt } from "./helper/readline";
 
 export default async function App() {
-  const name = await collectUserName();
+  const name = await prompt(messages.userName);
   let phone;
   let isValidPhone = false;
 
   while (!isValidPhone) {
-    phone = await collectUserPhone();
-    isValidPhone = await checkPhoneExistence(phone);
+    phone = (await prompt(messages.userPhone, {
+      type: "number",
+    })) as number;
+    isValidPhone = checkPhoneExistence(phone);
   }
 
   const data = { name, phone };
